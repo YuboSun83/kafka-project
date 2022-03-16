44118fd30c8db9dc12a2d05f368543815e23b66d
Yubo Sun <yubo.sun@ad.infosys.com>
2022-03-16 11:44:15 -0400

    updated Welcome page image and contents

--- kafka-react/src/components/Nav.jsx
+++ kafka-react/src/components/Nav.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import '../css/Navigator.css';
-import { Link }from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Navigator extends Component {
-    
-    
-    
+
     render() {
         return (
             <header className="header">
-                    <nav>
-                        <ul className="items">
-                            <li className="list"><Link to="/" className='item'>Home</Link></li>
-                            <li className="list"><Link to="/animations" className='item'>Graphic Demonstration</Link></li>
-                            <li className="list"><Link to="/about" className='item'>About Us</Link></li>
-                        </ul>
-                    </nav>
+                <nav>
+                    <ul className="items">
+                        <li className="list"><Link to="/" className='item'>Home</Link></li>
+                        <li className="list"><Link to="/animations" className='item'>Graphic Demonstration</Link></li>
+                        <li className="list"><Link to="/about" className='item'>About Us</Link></li>
+                    </ul>
+                </nav>
             </header>
         );
     }
--- kafka-react/src/components/Welcome.jsx
+++ kafka-react/src/components/Welcome.jsx
@@ -6,8 +6,20 @@ class Welcome extends Component {
 
     render() {
         return (
-            <div className='welcome'>
-                Welcome to learn Kafka
+            <div>
+                <div className='welcome'>
+                    Welcome to learn Kafka!
+                    <div className="doc_link">
+                        Offical Documentation:{" "}
+                        <a href='https://kafka.apache.org/documentation/'>
+                            https://kafka.apache.org/documentation/
+                        </a>
+                    </div>
+                </div>
+                <div className="basic_graph">
+                    <img src={require('../images/kafka_basic_graph.png')} alt=""/>
+                </div>
+                
             </div>
         );
     }
